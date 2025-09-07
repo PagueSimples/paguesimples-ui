7a57e66d90ac941b3d3373c3c5334c35ac56942d
TiagoMontes <tiagosantos6303@gmail.com>
2025-09-07 11:28:42 -0500

    feat: migrate Storybook to apps directory
    - Moved Storybook config to apps/storybook
    - Removed default example stories
    - Updated Storybook configuration for monorepo

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../packages/ui/src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@chromatic-com/storybook',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+};
+
+export default config;
